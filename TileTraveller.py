f5e2d5ce5042027de0da4f8b6459fb8f214287e2
Andri Freysson <andrifreys@Andris-MacBook-Pro.local>
2020-09-17 10:49:38 +0000

    Fourth commit

--- TileTraveller.py
+++ TileTraveller.py
@@ -3,3 +3,14 @@ This is a test!
 print("Hello World")
 
 þetta er breyting nr þrjú
+
+
+movement_input = int(input("Enter a direction: "))
+south = 0
+north = 0
+east = 0
+west = 0
+movement = 0
+
+
+if movement_input == "s" or movement_input == "S":   
